c1e54213a7b0c90a614b2e433ebb491cc95eae40
Brett Ahn <abu89@naver.com>
2024-06-25 23:27:13 +0900

    [init] initial project

---
+++ shedlock/src/test/java/com/study/shedlock/ShedlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.shedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShedlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
